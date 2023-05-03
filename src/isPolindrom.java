9d101c7f771a4a6d5d7b07f0ff757059d8675e1d
aykutalpp <aykutalpp@gmail.com>
2023-05-03 16:13:03 +0300

    ilk commit

---
+++ src/isPolindrom.java
@@ -0,0 +1,21 @@
+public class isPolindrom {
+    static boolean isPolindrom(int num){
+        int temp = num, reversedNum=0, lastNum;
+        while(temp!=0){
+            lastNum = temp % 10;
+            reversedNum = (reversedNum * 10) + lastNum;
+            temp /= 10;
+        }
+        if (reversedNum==num)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPolindrom(33));
+        System.out.println("Hello GitHub");
+    }
+
+}
