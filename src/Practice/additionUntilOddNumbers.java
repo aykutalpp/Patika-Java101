4fbf219db1fc9f3ca5c42ff2ec83b2e59ef59bea
aykutalpp <aykutalpp@gmail.com>
2023-05-11 13:44:15 +0300

    additionUntilOddNumbers eklendi

---
+++ src/Practice/additionUntilOddNumbers.java
@@ -0,0 +1,18 @@
+package Practice;
+import java.util.Scanner;
+
+public class additionUntilOddNumbers {
+    public static void main(String[] args) {
+        Scanner next = new Scanner(System.in);
+        int number,total = 0;
+        do {
+            System.out.print("Enter the number = ");
+            number = next.nextInt();
+
+            if (number % 2 == 0 && number % 4 == 0){
+                total += number;
+            }
+        }while (number % 2 == 0);
+        System.out.println("The sum of numbers entered that are divisible by both 2 and 4 = "+total);
+    }
+}
