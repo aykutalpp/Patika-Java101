82e04314a37f6804af2c73293960b8cd7867047e
aykutalpp <aykutalpp@gmail.com>
2023-05-17 16:49:59 +0300

    recursivePrime eklendi

---
+++ src/week2/Practice/recursivePrime.java
@@ -0,0 +1,29 @@
+package week2.Practice;
+import java.util.Scanner;
+
+public class recursivePrime {
+
+    static boolean isPrime(int n, int i)
+    {
+        // Base cases
+        if (n <= 1)
+            return false;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+
+        // Check for next divisor
+        return isPrime(n, i + 1);
+    }
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the number = ");
+        int n = inp.nextInt();
+
+        System.out.println(isPrime(n,2));
+
+
+
+    }
+}
